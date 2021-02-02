04e04c78802118a559264ebd4d25d6d8519fd196
sinam00692 <sina1070@gmail.com>
2021-02-02 20:06:34 +0330

    Add html errors

---
+++ code/app/Views/errors/html/error_404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="UTF-8">
+        <title>صفحه مورد نظر یافت نشد</title>
+    	<link rel="stylesheet" type="text/css" href="/theme/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/theme/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+    
+        <div class="backgroundLogin">
+            <div class="backgroundLoginDark"></div>
+            
+            <form class="boxLogin">
+                <h5>صفحه مورد نظر یافت نشد  - 404</h5>
+                <span>اگر مطمئن هستین که این صفحه وجود دارد ، با پشتیبانی تماس بگیرید</span>
+            </form>
+
+
+        </div>
+    
+    </body>
+</html>
