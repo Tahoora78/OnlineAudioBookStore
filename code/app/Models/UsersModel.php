a57373ae2518bb1065610289988ffafcf45f4577
arefehkompanii <awriik00@gmail.com>
2021-02-02 19:17:57 +0330

    Authentication

---
+++ code/app/Models/UsersModel.php
@@ -0,0 +1,44 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name','password','username'];
+    
+    public function create($name,$username,$password){
+        
+        $data=[
+            'name' => $name,
+            'username' => $username,
+            'password' => password_hash($password,PASSWORD_DEFAULT),
+        ];
+        
+        return $this->insert($data);
+    }
+    
+    
+    //--------------------------------------------------
+
+    
+    public function check_login($username,$password){
+        $user = $this->where('username', $username)->first();
+        if($user){
+        return password_verify($password, $user['password']) ? $user : false;
+        }else{
+        return false;
+        }
+        }
+    
+    
+    //--------------------------------------------------
+
+    
+    public function check_username($username){
+        return $this->where('username', $username)->select(['id'])->first();
+    }
+    
+    //--------------------------------------------------
+
+}
