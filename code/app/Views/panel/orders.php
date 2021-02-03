c0a2b965b4f6f4d35b7c505e47dff942c8d0767b
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-03 21:29:52 +0330

    Add orders view

---
+++ code/app/Views/panel/orders.php
@@ -0,0 +1,37 @@
+<?php $this->extend('layouts/panel') ?>
+
+<?php $this->section('title') ?>
+خرید های من
+<?php $this->endSection() ?>
+
+<?php $this->section('content') ?>
+
+<div class="widget-box">
+     <div class="wc-title">
+          <h4>خرید های من</h4>
+     </div>
+     <div class="widget-inner">
+          <div class="row">
+
+               <div class="col-md-2 col-lg-2 col-sm-6 m-b30">
+                    <div class="cours-bx">
+                         <a href="https://romankhan.com/book/3616/tond-bad">
+                              <div class="action-box">
+                                   <img src="https://romankhan.com/image/book/thumbnail/15879568199001.jpg" alt="">
+                              </div>
+                              <div class="info-bx text-center">
+                                   <span>تندباد</span>
+                              </div>
+                         </a>
+                    </div>
+               </div>
+
+               <div class="col-md-2 col-lg-2 col-sm-6 m-b30">
+                    
+               </div>
+
+          </div>
+     </div>
+</div>
+
+<?php $this->endSection() ?>
