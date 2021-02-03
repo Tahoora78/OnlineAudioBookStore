4e7e898cd0e7958d7173bd5912c924b5eb0ee168
arefehkompanii <49534520+arefehkompanii@users.noreply.github.com>
2021-02-03 20:07:58 +0330

    My books

---
+++ code/app/Views/panel/author.php
@@ -0,0 +1,37 @@
+<?php $this->extend('layouts/panel') ?>
+
+<?php $this->section('title') ?>
+کتاب های من - نویسنده
+<?php $this->endSection() ?>
+
+
+
+<?php $this->section('content') ?>
+
+<div class="widget-box">
+
+     <div class="wc-title">
+          <h4>کتاب های من - نویسنده</h4>
+     </div>
+
+     <div class="widget-inner">
+          <div class="row">
+               <?php foreach($book as $rows){ ?>
+               <div class="col-md-2 col-lg-2 col-sm-6 m-b30">
+                    <div class="cours-bx">
+                         <a href="<?php echo base_url('book/show/'.$rows['id']) ?>" target="_blank">
+                              <div class="action-box">
+                                   <img src="<?php echo base_url('/upload/cover/'.$rows['cover']); ?>" alt="">
+                              </div>
+                              <div class="info-bx text-center">
+                                   <span><?php echo $rows['title']; ?></span>
+                              </div>
+                         </a>
+                    </div>
+               </div>
+               <?php } ?>
+          </div>
+     </div>
+</div>
+
+<?php $this->endSection() ?>
