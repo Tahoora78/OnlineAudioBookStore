bce8dca3c056bc41a41c71a5781215089063e6fc
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-03 21:51:48 +0330

    Update income view

--- code/app/Views/panel/author_income.php
+++ code/app/Views/panel/author_income.php
@@ -23,7 +23,71 @@
           </div>
      </div>
      <!-- Your Profile Views Chart END-->
-    
+     <div class="col-lg-4 m-b30">
+          <div class="col-12">
+               <div class="widget-card widget-bg1">
+                    <div class="wc-item">
+                         <h4 class="wc-title">
+                              درآمد کل
+                         </h4>
+                         <span class="wc-des">
+                              تومان
+                         </span>
+                         <span class="wc-stats">
+                              <span class="counter">200000</span>
+                         </span>
+                    </div>
+               </div>
+          </div>
+          <div class="col-12">
+               <div class="widget-card widget-bg2">
+                    <div class="wc-item">
+                         <h4 class="wc-title">
+                             
+                              تعداد فروش
+                         </h4>
+                         <span class="wc-des">
+                              عدد
+                         </span>
+                         <span class="wc-stats ">
+                              <span class="counter">
+                                   120
+                              </span>
+                         </span>
+                    </div>
+               </div>
+          </div>
+          <div class="col-12">
+               <div class="widget-card widget-bg3">
+                    <div class="wc-item">
+                         <h4 class="wc-title">
+                         بازدید کل
+                         </h4>
+                         <span class="wc-des">
+                              بازدید
+                         </span>
+                         <span class="wc-stats counter">
+                              772
+                         </span>
+                    </div>
+               </div>
+          </div>
+          <div class="col-12">
+               <div class="widget-card widget-bg4">
+                    <div class="wc-item">
+                         <h4 class="wc-title">
+                              زمان کل کتاب صوتی
+                         </h4>
+                         <span class="wc-des">
+                             دقیقه
+                         </span>
+                         <span class="wc-stats counter">
+                              350
+                         </span>
+                    </div>
+               </div>
+          </div>
+     </div>
 </div>
 
 <?php $this->endSection() ?>
