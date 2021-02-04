d04e22094097d7db837c79f03c82572148f789cc
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-04 20:57:58 +0330

    Users buyed book

---
+++ code/app/Models/OrdersModel.php
@@ -0,0 +1,33 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class OrdersModel extends Model{
+    protected $table = 'orders';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['id_users','id_books','price'];
+    
+    public function create($id_users,$id_books,$price){
+        
+        $data=[
+          'id_users' => $id_users,
+          'id_books' => $id_books,
+          'price' => $price,
+     ];
+        
+        return $this->insert($data);
+    }
+    
+    
+    //--------------------------------------------------
+
+     public function show_AllByIdUsersIdBooks($id_users,$id_books){
+        return $this->where('id_users', $id_users)->where('id_books', $id_books)->first();
+     }
+
+     public function show_AllByIdUsersJoinBooks($id_users){
+          return $this->where('orders.id_users', $id_users)->join('books','orders.id_books = books.id')->select(['books.id','books.title','books.cover'])->findAll();
+     }
+
+
+}
