7fa5c0a67cee7289ef6b3e137fec8fa24ba132ca
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-04 20:59:17 +0330

    Show all books the users have

--- code/app/Controllers/Panel/Orders.php
+++ code/app/Controllers/Panel/Orders.php
@@ -1,14 +1,16 @@
 <?php namespace App\Controllers\Panel;
 
 use App\Controllers\BaseController;
+use App\Models\OrdersModel;
 
 class Orders extends BaseController
 {
 	public function index()
 	{
-		$this->auth();
-    
-          return view('panel/orders');
+		$auth = $this->auth();
+		$OrdersModel = new OrdersModel;
+		$book = $OrdersModel->show_AllByIdUsersJoinBooks($auth['id']);
+          return view('panel/orders',['book'=>$book]);
 
 	}
 
