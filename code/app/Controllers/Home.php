8fd374e887bcb337b26b45276a53f7f561718d65
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-05 14:46:38 +0330

    Update it

--- code/app/Controllers/Home.php
+++ code/app/Controllers/Home.php
@@ -1,10 +1,18 @@
 <?php namespace App\Controllers;
 
+use App\Models\BooksModel;
+
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('site/home');
+		$BooksModel = new BooksModel;
+		$book = $BooksModel->show_All();
+
+		$time_all = $BooksModel->sum_Time()['time'];
+		$views_all = $BooksModel->sum_Views()['views'];
+
+		return view('site/home' , ['book'=>$book,'time_all'=>$time_all,'views_all'=>$views_all]);
 	}
 
 	//--------------------------------------------------------------------
