21307fadf040ea1c467a5e71f65d952007e89707
sinam00692 <48389125+sinam00692@users.noreply.github.com>
2021-02-05 14:49:34 +0330

    Rules controller

---
+++ code/app/Controllers/Rules.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Rules extends BaseController
+{
+	public function index()
+	{
+		return view('site/rules');
+	}
+
+	//--------------------------------------------------------------------
+
+}
